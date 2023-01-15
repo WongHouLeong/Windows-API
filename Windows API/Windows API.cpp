ee927345ff2dbd67d7d36bb2b07c6bfdfb5a28cd
WongHouLeong <WongHouLeong@gmail.com>
2023-01-15 09:40:52 +0800

    API:MessageBoxA

--- Windows API/Windows API.cpp
+++ Windows API/Windows API.cpp
@@ -1,4 +1,10 @@
+#include <Windows.h>
 int main(int argc, char* argv[])
 {
+	MessageBoxA(NULL, "开始学习！", "老师", MB_OK | MB_ICONINFORMATION);
+	if (MessageBoxA(NULL, "这题谁会？", "老师", MB_YESNO | MB_ICONQUESTION) == IDYES)
+		MessageBoxA(NULL, "我会！", "学霸", MB_OK | MB_ICONQUESTION);
+	else
+		MessageBoxA(NULL, "不会！", "我", MB_OK | MB_ICONSTOP);
 	return 0;
 }
