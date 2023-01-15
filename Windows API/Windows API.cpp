e33e70239cc53f18fe7e8800fce1af5bceec89aa
WongHouLeong <WongHouLeong@gmail.com>
2023-01-15 14:12:01 +0800

    API:DialogBoxParamA

--- Windows API/Windows API.cpp
+++ Windows API/Windows API.cpp
@@ -1,10 +1,41 @@
 #include <Windows.h>
+#include "resource.h"
+
+INT_PTR CALLBACK Dlgproc(HWND hModule, UINT type, WPARAM wParam, LPARAM lParam)
+{
+	switch (type) {  //对话框初始化事件
+	case WM_INITDIALOG:
+	{
+		MessageBoxA(hModule, "初始化啦！", "提示", 0);
+		break;
+	}
+	case WM_COMMAND: { //触发的组件ID
+		switch (wParam)
+		{
+		case IDOK: { //确认
+			MessageBoxA(hModule, "确认！", "提示", 0);
+			break;
+		}
+		case IDCANCEL: {  //取消
+			MessageBoxA(hModule, "取消！", "提示", 0);
+			break;
+		}
+		default:
+			break;
+		}
+		break;
+	}
+	case WM_CLOSE: { //对话框关闭事件
+		break;
+	}
+	default://默认不做任何处理
+		break;
+	}
+	return false;
+}
+
 int main(int argc, char* argv[])
 {
-	MessageBoxA(NULL, "开始学习！", "老师", MB_OK | MB_ICONINFORMATION);
-	if (MessageBoxA(NULL, "这题谁会？", "老师", MB_YESNO | MB_ICONQUESTION) == IDYES)
-		MessageBoxA(NULL, "我会！", "学霸", MB_OK | MB_ICONQUESTION);
-	else
-		MessageBoxA(NULL, "不会！", "我", MB_OK | MB_ICONSTOP);
+	DialogBoxParamA(NULL, MAKEINTRESOURCE(IDD_DIALOG1), NULL, Dlgproc, 0);
 	return 0;
 }
